9972a254c0203f071288c61169f02e3fca439147
J.Montelon <jmontelon@gmail.com>
2022-10-05 17:26:49 +0000

    RPS w/ ruby - works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    
+    @some_number=rand(1000)
+    render({:template=>"games_templates/rock.html.erb"})
+    end
+
+  def play_scissor     
+      render({:template=>"games_templates/scissor.html.erb"})
+    end 
+
+  def play_paper    
+      render({:template=>"games_templates/paper.html.erb"})
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
 
+get("/rock", {:controller=>"application", :action=>"play_rock"})
+get("/scissor", {:controller=>"application", :action=>"play_scissor"})
+get("/paper", {:controller=>"application", :action=>"play_paper"})
+
+
 
 end
+
+#this is where we'll put all the URLs ppl can visit in our app. We'll use get("") - this method will have 2 arguments.
+
+#RUBY CLASS name and ruby method name. When somone visit rock,
